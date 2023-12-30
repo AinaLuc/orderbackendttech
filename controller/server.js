3a687b494d85069adcb83ded4c74888fe2f28092
AinaLuc <luaina.mada@gmail.com>
2023-12-30 10:30:02 +0300

    server

--- controller/server.js
+++ controller/server.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017/test', {
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1')
   .then(() => {
     console.log('Connected to MongoDB');
   })
