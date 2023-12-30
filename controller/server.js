91e13c71dbb666b199fe7c443131106e5ed0028c
AinaLuc <luaina.mada@gmail.com>
2023-12-30 09:55:54 +0300

    mongoose error

--- controller/server.js
+++ controller/server.js
@@ -22,10 +22,8 @@ mongoose.connect('mongodb://localhost:27017/test', {
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/test');
+
 
 // Event: Error during connection
 mongoose.connection.on('error', (error) => {
